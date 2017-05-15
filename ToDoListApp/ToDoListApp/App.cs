9628ac51acb292ea10d4f1143d0f5d8892acc73c
radhikaprabu <radhikaprabu91699@gmail.com>
2017-05-15 17:27:27 -0500

    Made some changes

--- ToDoListApp/ToDoListApp/App.cs
+++ ToDoListApp/ToDoListApp/App.cs
@@ -16,14 +16,8 @@ namespace ToDoListApp
 
         public App()
         {
-            //Resources = new ResourceDictionary();
-            //Resources.Add("primaryGreen", Color.FromHex("91CA47"));
-            //Resources.Add("primaryDarkGreen", Color.FromHex("6FA22E"));
-
             var nav = new NavigationPage(new ToDoListPage());
-            //nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
-            //nav.BarTextColor = Color.White;
-
+            
             MainPage = nav;
         }
 
